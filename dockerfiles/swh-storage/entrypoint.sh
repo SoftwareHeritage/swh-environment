bf6ed65cdde6078815fc7ff25e3a6abd2c826b03
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-12-13 19:45:36 +0100

    swh-storage/entrypoint.sh: Remove tabs and use spaces

--- dockerfiles/swh-storage/entrypoint.sh
+++ dockerfiles/swh-storage/entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [[ -d /src ]] ; then
-	for srcrepo in /src/swh-* ; do
-		pushd $srcrepo
-		pip install -e .
-		popd
-	done
+    for srcrepo in /src/swh-* ; do
+        pushd $srcrepo
+        pip install -e .
+        popd
+    done
 fi
 
 echo "${PGHOST}:5432:${POSTGRES_DB}:${PGUSER}:${POSTGRES_PASSWORD}" > ~/.pgpass
@@ -22,14 +22,14 @@ EOF
 chmod 0400 ~/.pgpass
 
 case "$1" in
-	"shell")
-		exec bash -i
-		;;
-	"init")
-		echo Setup the database
-		swh-db-init storage --db-name ${POSTGRES_DB} --no-create True
-		;;
-	*)
-		echo Starting the swh-storage API server
-		exec python -m swh.storage.api.server /storage.yml
+    "shell")
+        exec bash -i
+        ;;
+    "init")
+        echo Setup the database
+        swh-db-init storage --db-name ${POSTGRES_DB} --no-create True
+        ;;
+    *)
+        echo Starting the swh-storage API server
+        exec python -m swh.storage.api.server /storage.yml
 esac
