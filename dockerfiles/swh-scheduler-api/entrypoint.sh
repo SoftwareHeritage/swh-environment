a377e8701fbaab80c21f48433e31f2ddbe893ad3
David Douard <david.douard@sdfa3.org>
2018-12-14 14:26:05 +0100

    Add scheduler services
    - scheduler-api
    - scheduler-listener
    - scheduler-runner
    
    this comes with a dedicated database and a rabbitmq service.

---
+++ dockerfiles/swh-scheduler-api/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d /src ]] ; then
+    for srcrepo in /src/swh-* ; do
+        pushd $srcrepo
+        pip install -e .
+        popd
+    done
+fi
+
+echo "${PGHOST}:5432:${POSTGRES_DB}:${PGUSER}:${POSTGRES_PASSWORD}" > ~/.pgpass
+cat > ~/.pg_service.conf <<EOF
+[swh]
+dbname=${POSTGRES_DB}
+host=${PGHOST}
+port=5432
+user=${PGUSER}
+EOF
+
+chmod 0400 ~/.pgpass
+
+case "$1" in
+    "shell")
+        exec bash -i
+        ;;
+    *)
+		echo Waiting for postgresql to start
+		until psql service=swh -c "select 1" 2>&1 > /dev/null; do sleep 0.1; done
+
+        echo Setup the swh-scheduler API database
+        PGPASSWORD=${POSTGRES_PASSWORD} swh-db-init scheduler \
+				  --db-name ${POSTGRES_DB}
+
+        echo Starting the swh-scheduler API server
+        exec python -m swh.scheduler.api.server /scheduler.yml
+esac
---
+++ dockerfiles/swh-scheduler-worker/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d /src ]] ; then
+    for srcrepo in /src/swh-* ; do
+        pushd $srcrepo
+        pip install -e .
+        popd
+    done
+fi
+
+echo "${PGHOST}:5432:${POSTGRES_DB}:${PGUSER}:${POSTGRES_PASSWORD}" > ~/.pgpass
+cat > ~/.pg_service.conf <<EOF
+[swh]
+dbname=${POSTGRES_DB}
+host=${PGHOST}
+port=5432
+user=${PGUSER}
+EOF
+
+chmod 0400 ~/.pgpass
+
+case "$1" in
+    "shell")
+        exec bash -i
+        ;;
+    "listener")
+        echo Starting the swh-scheduler listener
+        exec python -m swh.scheduler.celery_backend.listener
+		;;
+    "runner")
+        echo Starting the swh-scheduler runner
+        exec sh -c 'while true; do
+            echo running pending tasks at `/bin/date`;
+            python -m swh.scheduler.celery_backend.runner;
+            sleep 10;
+          done'  # beuark
+		;;
+	*)
+		echo "Provide a command (shell|listener|runner)"
+		exit 1
+		;;
+esac
