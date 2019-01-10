4c5eea37d25e31710b6c73d5095f3cbee0800876
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-01-10 16:56:13 +0100

    Add swh-indexer-journal-client.

---
+++ dockerfiles/swh-indexer-journal-client/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d /src ]] ; then
+    for srcrepo in /src/swh-* ; do
+      pushd $srcrepo
+      pip install -e .
+      popd
+    done
+fi
+
+echo Installed Python packages:
+pip list
+
+case "$1" in
+    "shell")
+      exec bash -i
+      ;;
+    *)
+      echo "Starting swh-journal client"
+      python3 -m swh.indexer.journal_client
+      ;;
+esac
+
