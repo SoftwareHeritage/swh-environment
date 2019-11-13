d1992ffd42abf7fb26db40b07668a0ff3d44078e
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-11-13 14:59:53 +0100

    Add swh-search.
    Note that swh-web does not use it for now.

---
+++ services/swh-search/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+source /srv/softwareheritage/utils/pyutils.sh
+setup_pip
+
+case "$1" in
+    "shell")
+      exec bash -i
+      ;;
+    *)
+      echo Starting the swh-search API server
+      wait-for-it elasticsearch:9200 -s --timeout=0
+      echo "Waiting for ElasticSearch cluster to be up"
+      cat << EOF | python3
+import elasticsearch
+es = elasticsearch.Elasticsearch(['elasticsearch:9200'])
+es.cluster.health(wait_for_status='yellow')
+EOF
+      echo "ElasticSearch cluster is up"
+      swh search -C $SWH_CONFIG_FILENAME initialize
+      exec gunicorn --bind 0.0.0.0:5010 \
+           --reload \
+           --threads 4 \
+           --workers 2 \
+           --log-level DEBUG \
+           --timeout 3600 \
+           'swh.search.api.server:make_app_from_configfile()'
+      ;;
+esac
