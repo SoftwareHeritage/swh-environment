2eb7a2c4407d001fe85c6a7c3a4b5d74179f4fa5
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-09 18:06:32 +0100

    docker-compose.search.yml: Add journal client for indexed values
    This will allow to run a search journal client subscribed to
    `swh.journal.indexed.origin_intrinsic_metadata` topic
    
    Related to D4701

--- docker/services/swh-search-journal-client/entrypoint.sh
+++ docker/services/swh-search-journal-client/entrypoint.sh
@@ -10,10 +10,9 @@ case "$1" in
       exec bash -i
       ;;
     *)
-      echo "Starting swh-indexer-journal client"
+      echo "Starting swh-search-journal client"
       exec wait-for-it -s kafka:9092 -s --timeout=0 -- \
           wait-for-it -s swh-search:5010 -s --timeout=0 -- \
-          swh --log-level DEBUG search --config-file /etc/softwareheritage/search/journal_client_objects.yml journal-client objects
+          swh --log-level DEBUG search --config-file /etc/softwareheritage/search/journal_client.yml journal-client objects
       ;;
 esac
-
