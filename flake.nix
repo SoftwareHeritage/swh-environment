025a2d7bc4212400b6a80fbc404cea14c5695fa1
Antoine Martin <antoine@alarsyo.net>
2021-08-13 15:14:00 +0200

    flake: move third party packages below

--- flake.nix
+++ flake.nix
@@ -18,6 +18,47 @@
           defaultPackage = self.packages.${system}.swh-model;
 
           packages = {
+            swh-model = pkgs.python3Packages.buildPythonPackage rec {
+              pname = "swh.model";
+              version = "2.8.0";
+
+              src = self.inputs.swh-model;
+
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                attrs
+                click
+                dateutil
+                deprecated
+                dulwich
+                hypothesis
+                iso8601
+                pytest
+                pytz
+                setuptools
+                typing-extensions
+
+                self.packages.${system}.attrs-strict
+              ];
+
+              # HACK: flakes don't include the `.git/` folder as part of the
+              #       source, so setuptools fails because it can't identify the
+              #       version this way, so we provide it explicitely.
+              prePatch = ''
+                substituteInPlace setup.py \
+                  --replace 'setup_requires=["setuptools-scm"],' "" \
+                  --replace "use_scm_version=True" 'version="${version}"'
+              '';
+
+              meta = with pkgs.lib; {
+                homepage = "https://forge.softwareheritage.org/source/swh-model/";
+                license = licenses.gpl3Only;
+              };
+            };
+
+
+            ###
+            # TODO: third party packages below, upstream to nixpkgs?
+            ###
             attrs-strict = pkgs.python3Packages.buildPythonPackage rec {
               pname = "attrs_strict";
               version = "0.2.1";
@@ -68,42 +109,6 @@
               };
             };
 
-            swh-model = pkgs.python3Packages.buildPythonPackage rec {
-              pname = "swh.model";
-              version = "2.8.0";
-
-              src = self.inputs.swh-model;
-
-              propagatedBuildInputs = with pkgs.python3Packages; [
-                attrs
-                click
-                dateutil
-                deprecated
-                dulwich
-                hypothesis
-                iso8601
-                pytest
-                pytz
-                setuptools
-                typing-extensions
-
-                self.packages.${system}.attrs-strict
-              ];
-
-              # HACK: flakes don't include the `.git/` folder as part of the
-              #       source, so setuptools fails because it can't identify the
-              #       version this way, so we provide it explicitely.
-              prePatch = ''
-                substituteInPlace setup.py \
-                  --replace 'setup_requires=["setuptools-scm"],' "" \
-                  --replace "use_scm_version=True" 'version="${version}"'
-              '';
-
-              meta = with pkgs.lib; {
-                homepage = "https://forge.softwareheritage.org/source/swh-model/";
-                license = licenses.gpl3Only;
-              };
-            };
           };
         });
 }
