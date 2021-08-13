5f5724f5dbbc8e170d234f6c1fb1967a8dce4644
Antoine Martin <antoine@alarsyo.net>
2021-08-13 15:28:36 +0200

    flake: package swh-core

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
+    swh-core = {
+      url = "github:SoftwareHeritage/swh-core";
+      flake = false;
+    };
     swh-model = {
       url = "github:SoftwareHeritage/swh-model";
       flake = false;
@@ -15,9 +19,80 @@
         pkgs = import nixpkgs { inherit system; };
       in
         {
-          defaultPackage = self.packages.${system}.swh-model;
+          defaultPackage = self.packages.${system}.swh-core;
 
           packages = {
+            swh-core = pkgs.python3Packages.buildPythonPackage rec {
+              pname = "swh.core";
+              version = "0.14.4";
+
+              src = self.inputs.swh-core;
+
+              # FIXME: tests disabled for now, some packages missing in nixpkgs
+              #        (types-click for instance, which shouldn't be needed
+              #        anymore since click v8.0 which ships type annotations
+              #        directly) (PR to upstream?)
+              doCheck = false;
+
+              /*
+
+              buildInputs = with pkgs.python3Packages; [
+                # requirements-test.txt
+                hypothesis
+                pre-commit
+                pytest
+                pytest-mock
+                pytz
+                requests-mock
+                types-click
+                types-flask
+                types-pytz
+                types-pyyaml
+                types-requests
+
+                # requirements-db-pytestplugin.txt
+                pytest-postgresql
+              ];
+
+              */
+
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                # requirements.txt
+                click
+                deprecated
+                pyyaml
+                sentry-sdk
+
+                # requirements-db.txt
+                psycopg2
+                typing-extensions
+
+                # requirements-http.txt
+                aiohttp
+                blinker
+                flask
+                iso8601
+                msgpack
+                requests
+
+                self.packages.${system}.aiohttp-utils
+              ];
+
+              # HACK: flakes don't include the `.git/` folder as part of the
+              #       source, so setuptools fails because it can't identify the
+              #       version this way, so we provide it explicitely.
+              prePatch = ''
+                substituteInPlace setup.py \
+                  --replace 'setup_requires=["setuptools-scm"],' "" \
+                  --replace "use_scm_version=True" 'version="${version}"'
+              '';
+
+              meta = with pkgs.lib; {
+                homepage = "https://forge.softwareheritage.org/source/swh-core/";
+                license = licenses.gpl3Only;
+              };
+            };
+
             swh-model = pkgs.python3Packages.buildPythonPackage rec {
               pname = "swh.model";
               version = "2.8.0";
