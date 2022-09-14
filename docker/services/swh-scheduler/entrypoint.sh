ffd82bc7e0fbd38521d37465ba9cb34d7b8132ea
Antoine Lambert <anlambert@softwareheritage.org>
2022-09-14 16:59:56 +0200

    docker: Add swh-scheduler-update-metrics service
    It enables to update the metrics regarding the number of loaded
    origins into the archive.
    
    Those are consumed by the archive coverage widget in the webapp
    homepage.

--- docker/services/swh-scheduler/entrypoint.sh
+++ docker/services/swh-scheduler/entrypoint.sh
@@ -9,31 +9,43 @@ setup_pgsql
 setup_pip
 
 
-if [ "$1" = 'shell' ] ; then
-	shift
-	if (( $# == 0)); then
-		exec bash -i
-	else
-		"$@"
-	fi
-else
-    wait_pgsql ${POSTGRES_DB}
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    "update-metrics")
+        wait-for-it swh-scheduler:5008 -s --timeout=0
 
-    echo swh-scheduler database setup
+        echo "Start periodic scheduler metrics update routine (in background)"
+        exec sh -c 'trap exit TERM INT; while :; do
+        (date && swh scheduler origin update-metrics)
+        sleep 60 &
+        wait ${!}
+        done'
+        ;;
+    *)
+        wait_pgsql ${POSTGRES_DB}
 
-    echo Creating extensions...
-    swh db init-admin --db-name ${POSTGRES_DB} scheduler
+        echo swh-scheduler database setup
 
-    echo Initializing the database...
-    swh db init --db-name ${POSTGRES_DB} scheduler
+        echo Creating extensions...
+        swh db init-admin --db-name ${POSTGRES_DB} scheduler
 
-    echo Starting the swh-scheduler API server
-    exec gunicorn --bind 0.0.0.0:5008 \
-         --log-level DEBUG \
-         --threads 2 \
-         --workers 2 \
-         --reload \
-         --timeout 3600 \
-         --config 'python:swh.core.api.gunicorn_config' \
-         'swh.scheduler.api.server:make_app_from_configfile()'
-fi
+        echo Initializing the database...
+        swh db init --db-name ${POSTGRES_DB} scheduler
+
+        echo Starting the swh-scheduler API server
+        exec gunicorn --bind 0.0.0.0:5008 \
+            --log-level DEBUG \
+            --threads 2 \
+            --workers 2 \
+            --reload \
+            --timeout 3600 \
+            --config 'python:swh.core.api.gunicorn_config' \
+            'swh.scheduler.api.server:make_app_from_configfile()'
+esac
