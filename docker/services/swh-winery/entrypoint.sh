9a5127c644532515c4cd8170dae09a054b110063
Loïc Dachary <loic@dachary.org>
2023-01-06 16:46:32 +0000

    Generated commit for differential D6834

---
+++ docker/services/swh-winery/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+source /srv/softwareheritage/utils/pyutils.sh
+setup_pip
+
+echo Installed Python packages:
+pip list
+
+if [ "$1" = 'shell' ] ; then
+    exec bash -i
+else
+    echo Starting the swh-winery API server
+  exec gunicorn --bind 0.0.0.0:5012 \
+       --worker-class aiohttp.worker.GunicornWebWorker \
+       --log-level DEBUG \
+       --threads 4 \
+       --workers 2 \
+       --reload \
+       --timeout 3600 \
+       --config 'python:swh.core.api.gunicorn_config' \
+       'swh.objstorage.api.server:make_app_from_configfile()'
+
+fi
