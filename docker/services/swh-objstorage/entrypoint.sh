529ac79e42701a2fe9004f6733ebb329edfe2feb
David Douard <david.douard@sdfa3.org>
2023-10-04 10:59:28 +0200

    docker: make the objstorage replayer wait for kafka to be ready

--- docker/services/swh-objstorage/entrypoint.sh
+++ docker/services/swh-objstorage/entrypoint.sh
@@ -15,6 +15,8 @@ case "$1" in
 	  shift
       wait-for-it swh-objstorage:5003
       wait-for-it swh-objstorage-mirror:5003
+	  wait-for-it kafka:9092 -s --timeout=0
+	  wait-for-it kafka-rest:8082 -s --timeout=0
       echo "Starting the SWH mirror content replayer"
       exec swh --log-level ${LOG_LEVEL:-WARNING} \
            objstorage replay $@
