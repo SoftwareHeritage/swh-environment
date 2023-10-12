c57c4bdeed1c8dcd4a087a4630a0210530d2ab26
David Douard <david.douard@sdfa3.org>
2023-10-12 10:13:29 +0200

    docker: do not copy mounted src in /tmp, seems now unnecessary
    Note that it comes with the removal of swh-web's specific yarn install
    scaffolding; this will not work anymore in ro mounter src directories
    (which is the desired way of mounting src dirs in the container), and
    since we now install the src package in editable mode, for the case of
    a user actually hacking the web assets in a docker test env, rebuilding
    the assets can be handled from the hosts.

--- docker/utils/pyutils.sh
+++ docker/utils/pyutils.sh
@@ -7,31 +7,9 @@ setup_pip () {
     tmpdir=`mktemp -d`
     pushd /src
     for srcrepo in swh-* ; do
-      if [ ! -w $srcrepo ]; then
-        echo "$srcrepo is read-only; install from a local copy"
-        # Source directories might not be writeable, but building them writes
-        # in-tree; so we're copying them to a location guaranteed to be writeable.
-        rsync -a --chmod=+w $srcrepo $tmpdir/ --exclude "*/__pycache__/" --exclude "*/.tox/" --exclude "*/.hypothesis/"
-        pushd $tmpdir/$srcrepo
-      else
-        pushd $srcrepo
-      fi
+      pushd $srcrepo
       # Install package in editable mode if source directory is writable
       pip install -e .
-      # swh-web special case to handle frontend assets compilation
-      if [ -f package.json ]; then
-        yarn install --frozen-lockfile
-        if [ ! -w . ]; then
-          # swh-web source directory is not writable, there is no interest to
-          # use webpack-dev-server as we made a copy of assets source files,
-          # simply compile the assets then
-          yarn build-dev
-        else
-          # webpack-dev-server can be used, web application will be automatically
-          # reloaded in the browser when modifying assets sources (js, css, ...)
-          yarn start-dev&
-        fi
-      fi
       popd
     done
     popd
